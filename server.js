6e0492da291dd67406f042ad1e0e4e35df57909e
GradedJestRisk <graded.jest.risk@gmail.com>
2020-12-08 08:52:11 +0100

    Enable deployment on Scalingo

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const Hapi = require('@hapi/hapi');
 
 const init = async () => {
 
+    const port = parseInt(process.env.PORT, 10) || 3000;
+
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port
     });
 
 
